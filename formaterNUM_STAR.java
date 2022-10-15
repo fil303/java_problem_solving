253fc318da364656dec36b9faccb6e34f9415671
fil303 <55945436+fil303@users.noreply.github.com>
2022-10-15 15:47:31 +0000

    formater number and star

---
+++ formaterNUM_STAR.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+       for (int i = 1 ; i <= 6 ; i++){
+           for (int j = 1 ; j <= i ; j++){
+               System.out.print(j);
+           }
+           for (int k = 6 ; k > i ; k--){
+               System.out.print("*");
+           }
+           System.out.println("");
+       }
+    }
+}
+//  1*****
+//  12****
+//  123***
+//  1234**
+//  12345*
+//  123456
